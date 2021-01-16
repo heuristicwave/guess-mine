a8934e384ca28fa98b110bf7f5b38b8c596e0372
heuristicwave <31366038+heuristicwave@users.noreply.github.com>
2021-01-17 00:10:13 +0900

    Setting Up Socket IO

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # Guess Mine
-Realtime Drawing Game built with SocketIO, gulp and node
+Realtime Drawing Game built with [SocketIO](https://socket.io/), gulp and node
+
+<br>
+
+## Concepts
+Socket.IO is a library that enables real-time, bidirectional and **event-based** communication between the browser and the server.
+
+<br>
+
+### Command
+
+```shell
+yarn dev:server
+```
+
+<br>
+
+### Shortcuts
+
+create pug skeleton code
+```
+html:5 + tab
+```
--- src/server.js
+++ src/server.js
@@ -1,1 +1,17 @@
+import { join } from "path";
 import express from "express";
+import socketIO from "socket.io";
+
+const PORT = 5000;
+const app = express();
+app.set("view engine", "pug");
+app.set("views", join(__dirname, "views"));
+app.use(express.static(join(__dirname, "static")));
+app.get("/", (req, res) => res.render("home"));
+
+const handleListening = () =>
+  console.log(`✅ Server running: http://localhost:${PORT}`);
+
+const server = app.listen(PORT, handleListening); // socket on server
+
+const io = socketIO(server);
