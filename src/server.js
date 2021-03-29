a55a9aa26543bd7706f66b0b3f0f4c8e1004b93c
heuristicwave <31366038+heuristicwave@users.noreply.github.com>
2021-03-29 23:48:28 +0900

    Add socket io test

--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
 import { join } from "path";
 import express from "express";
 import socketIO from "socket.io";
+import logger from "morgan";
 
 const PORT = 5000;
 const app = express();
 app.set("view engine", "pug");
 app.set("views", join(__dirname, "views"));
+app.use(logger("dev"));
 app.use(express.static(join(__dirname, "static")));
 app.get("/", (req, res) => res.render("home"));
 
@@ -14,4 +16,13 @@ const handleListening = () =>
 
 const server = app.listen(PORT, handleListening); // socket on server
 
-const io = socketIO(server);
+const io = socketIO.listen(server);
+
+let sockets = [];
+
+io.on("connection", (socket) => {
+  sockets.push(socket.id);
+});
+
+// show sockets
+// setInterval(() => console.log(sockets), 1000);
